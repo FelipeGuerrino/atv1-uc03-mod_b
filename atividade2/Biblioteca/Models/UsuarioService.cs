26f1c61b6a79369998c2b0a94de8dad7bf4faee2
FelipeGuerrino <felipe_bg2005@hotmail.com>
2022-02-19 16:35:54 -0300

    tabela usuarios criada por ef core (models Usuario e UsuarioService)

---
+++ atividade2/Biblioteca/Models/UsuarioService.cs
@@ -0,0 +1,50 @@
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Biblioteca.Models
+{
+    public class UsuarioService 
+    {
+        public void Inserir(Usuario u)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                bc.Usuarios.Add(u);
+                bc.SaveChanges();
+            }
+        }
+
+        public void Atualizar(Usuario u)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                Usuario user = bc.Usuarios.Find(u.Id);
+                user.Nome = u.Nome;
+                user.Senha = u.Senha;
+                
+                bc.SaveChanges();
+            }
+        }
+
+        public ICollection<Usuario> Listar()
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                IQueryable<Usuario> query;
+
+                query = bc.Usuarios;
+
+                return query.OrderBy(u => u.Nome).ToList();
+            }
+        }
+
+        public Usuario ObterPorId(int id)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                return bc.Usuarios.Find(id);
+            }
+        }
+    }
+}
